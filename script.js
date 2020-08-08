b3a1eb1c8a4b6d67ad73d0a1c5edaf5e9c24a53e
Johnny Lieu <johnnylieu@yahoo.com>
2020-08-07 22:27:33 -0700

    tried to hide api lol

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 var searchBtn = $("#searchBtn");
+var savedLocations = [];
 
 // when clicking the search button, should grab data and push to local storage
 searchBtn.on("click", function(event) {
+    event.preventDefault();
     var button = $(this);
     console.log(button); //working
 
     var city = $("#searchInput").val().trim();
     console.log(city); // working, grabbing data from search form
 
-    queryURL = "pro.openweathermap.org/data/2.5/forecast/hourly?id=" + city +"&appid=8f775258afdec054195f89c38855f678"
+    // localStorage.setItem(city); // needs second argument
+
+    var noneYo = "8f775258afdec054195f89c38855f678"
+
+    queryURL = "pro.openweathermap.org/data/2.5/forecast/hourly?id=" + city +"&appid=" + noneYo;
     $.ajax({
         url: queryURL,
         method: "GET"
