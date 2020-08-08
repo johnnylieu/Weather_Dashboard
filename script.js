afcc11b90a7f3d0307f582e83d5dd8d3ed80f75d
Johnny Lieu <johnnylieu@yahoo.com>
2020-08-07 18:12:15 -0700

    trying to pull button data

--- script.js
+++ script.js
@@ -4,4 +4,17 @@ searchBtn.on("click", function(event) {
     var button = $(this);
     console.log(button); //working
 
+    var btnData = $(this).attr("#searchInput");
+    console.log(btnData);
+
+    queryURL = "pro.openweathermap.org/data/2.5/forecast/hourly?id=" + btnData +"&appid=8f775258afdec054195f89c38855f678"
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        // currentLoc = response.name;
+        // saveLoc(response.name);
+        // getCurrent(currentLoc);
+    });
+
 });
