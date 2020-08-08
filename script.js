a99d0831c9fa326f574c89433acba35633d1cc3b
Johnny Lieu <johnnylieu@yahoo.com>
2020-08-07 18:21:57 -0700

    grabbing data from search form

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 var searchBtn = $("#searchBtn");
 
+// when clicking the search button, should grab data and push to local storage
 searchBtn.on("click", function(event) {
     var button = $(this);
     console.log(button); //working
 
-    var btnData = $(this).attr("#searchInput");
-    console.log(btnData);
+    var city = $("#searchInput").val().trim();
+    console.log(city); // working, grabbing data from search form
 
     queryURL = "pro.openweathermap.org/data/2.5/forecast/hourly?id=" + btnData +"&appid=8f775258afdec054195f89c38855f678"
     $.ajax({
