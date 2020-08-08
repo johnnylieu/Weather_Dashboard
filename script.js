572a300a05df090fb08e0cda64290be343e01d46
Johnny Lieu <johnnylieu@yahoo.com>
2020-08-08 13:24:59 -0700

    response working

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ searchBtn.on("click", function(event) {
         $("#prevSearches").prepend("<p>" + (savedLocations[i]) + "</p>");
     };
 
-    queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=8f775258afdec054195f89c38855f678";
+    queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=8f775258afdec054195f89c38855f678";
     $.ajax({
         url: queryURL,
         method: "GET"
